eeedfe78d89cb8222c06f3808e0966a1488af0b4
MatthiasKainer <mail@matthias-kainer.de>
2015-04-16 12:15:42 +0200

    triggering the gruntfile from an itbldz entry point now. Still not sure how to make the startup nicer than in version 1 though

---
+++ itbldz.ts
@@ -0,0 +1,11 @@
+﻿process.title = 'itbldz';
+
+global.basedir = process.cwd();
+global.relativeDir = __dirname;
+global.requireRoot = function (name) {
+    return require(__dirname + '/' + name);
+};
+
+import grunt = require('grunt');
+import path = require('path');
+require(path.join(global.relativeDir, "src/grunt-utils/gruntfile.js"))(grunt);
--- src/grunt-utils/gruntfile.js
+++ src/grunt-utils/gruntfile.js
@@ -1,8 +1,4 @@
-﻿global.requireRoot = function (name) {
-    return require(__dirname + '/' + name);
-};
-
-module.exports = function (grunt) {
+﻿module.exports = function (grunt) {
     var tasks = [];
     grunt.registerTask('default', tasks);
 };
