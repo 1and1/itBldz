cce3a6f80dd55373d916bfc093d9462cdf323e6b
MatthiasKainer <mail@matthias-kainer.de>
2015-11-25 09:09:44 +0100

    Must not mix move to es6 with feature branches
    Must not mix move to es6 with feature branches
    Must not mix move to es6 with feature branches

--- src/lib/arguments.ts
+++ src/lib/arguments.ts
@@ -1,8 +1,21 @@
 export default class Arguments {
-    public static getConfigArgument(value : string, defaultExtension : string = ".json") : string {
-        if (value.endsWith(".json"))
+    /**
+     * Polyfill https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+     */
+    private static endsWith(searchString: string, position: number = undefined) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    }
+
+    public static getConfigArgument(value: string, defaultExtension: string = ".json"): string {
+        if (Arguments.endsWith(".json"))
             return value;
-        if (value.endsWith(".yml"))
+        if (Arguments.endsWith(".yml"))
             return value;
         return value + defaultExtension;
     }
