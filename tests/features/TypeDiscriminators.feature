0720b940cd76cd706e0ef778b5a3e8a49375ab79
MatthiasKainer <mail@matthias-kainer.de>
2016-01-29 15:28:14 +0100

    Added tests for calling methods from serialized values

--- tests/features/TypeDiscriminators.feature
+++ tests/features/TypeDiscriminators.feature
@@ -1,17 +1,32 @@
-Feature: Extending a configuration with modules
+Feature: Extending a configuration with type discriminators
 	As a user I want to be able to use simple functions in my modules
 	So I can extend it without creating a grunt-task every time
 
-Background:
+Scenario: Call a function of an existing grunt task
+	Given I have a src directory with a file "test.js"
+	And I have an empty target directory
+    Given I have a file "function.js" with the following content
+    """
+function rename(dest, src) {
+    return dest + "/result.txt";
+}
+    """
+    Given I have an empty build file
+		And the build has the build steps "deploy"
+			And the build step "deploy" has a task group "files"
+				And the task group "files" in the build step "deploy" has a task runner that copies the src directory to the target directory and calls the function "rename" from script "function.js"
+    And the build file is in the root of my application
+	When I execute the build command    
+	When I execute a custom build command with argument "--verbose --stack"
+	Then the file "result.txt" should exist in folder "target"
+
+Scenario: Typediscriminator for module
 	Given I have an empty modules file
-		And the module HelloWorld is defined and exists   
+		And the module HelloWorld is defined and exists
 	And the modules file is in the root of my application
 	Given I have an empty build file
 		And the build has the build steps "echo"
 			And the build step echo has a exec task runner with a type discriminator for the HelloWorld Module with "Bruce Lee" as actor
 	And the build file is in the root of my application
-
-Scenario: 
-	Trigger a build
 	When I execute the build command
 	Then the message "Hello Bruce Lee" should appear on the command line
--- tests/features/step_definitions/definitions.js
+++ tests/features/step_definitions/definitions.js
@@ -1,29 +1,29 @@
 ﻿var runningBuild = function () {
     this.World = require("../support/World").World;
     require('chai').should();
-    
+
     var fs = require('fs');
-    
+
     var setupFile;
     var config;
     var modules;
     var vars;
     var scenarios = {};
-    
+
     this.Given(/^I have a src directory with a file "([^"]*)"$/, function (fileName, callback) {
         this.fileSystem.withEmptyDirectory(".", callback);
         this.fileSystem.withFileInDirectory(fileName, "src", callback);
     });
-    
+
     this.Given(/^I have an empty target directory$/, function (callback) {
         this.fileSystem.withEmptyDirectory("target", callback);
     });
-    
+
     this.Given(/^I have an empty build file$/, function (callback) {
         config = {};
         callback();
     });
-    
+
     this.Given(/^I have an empty modules file$/, function (callback) {
         modules = [];
         callback();
@@ -33,42 +33,46 @@
         vars = "";
         callback();
     });
-    
+
     this.Given(/^I have a build scenario file "([^"]*)"$/, function (name, callback) {
-      scenarios[name] = "steps:";
-      callback();
+        scenarios[name] = "steps:";
+        callback();
     });
 
     this.Given(/^the scenario "([^"]*)" executes the step "([^"]*)"$/, function (name, step, callback) {
-      scenarios[name] += "\n- \"" + step + "\"";
-      callback();
+        scenarios[name] += "\n- \"" + step + "\"";
+        callback();
     });
-        
+
     this.Given("the build file is in the root of my application", function (callback) {
         this.fileSystem.withFileWithContentInDirectory("build.json", JSON.stringify(config), ".", callback);
     });
-        
+
     this.Given(/the scenario file "([^"]*)" is in the root of my application$/, function (name, callback) {
         this.fileSystem.withFileWithContentInDirectory(name + ".yml", scenarios[name], ".", callback);
     });
-    
+
     this.Given("the modules file is in the root of my application", function (callback) {
         this.fileSystem.withFileWithContentInDirectory("modules.json", JSON.stringify(modules), ".", callback);
     });
-    
+
     this.Given(/^the build file is in the root of my application with the name "([^"]*)"$/, function (name, callback) {
         this.fileSystem.withFileWithContentInDirectory(name, JSON.stringify(config), ".", callback);
     });
-    
+
+    this.Given(/^I have a file "([^"]*)" with the following content$/, function (name, content, callback) {
+        this.fileSystem.withFileWithContentInDirectory(name, content, ".", callback);
+    });
+
     this.Given(/^the build has the build steps "([^"]*)"$/, function (stepName, callback) {
         config[stepName] = {};
         callback();
     });
-    
+
     this.Given(/^the build step "([^"]*)" has a task runner that cleans the target folder$/, function (stepName, callback) {
         var targetFolder = this.fileSystem.getFullDirectory("target");
         config[stepName]["clean"] = {
-            "task" : "clean",
+            "task": "clean",
             "package": "grunt-contrib-clean",
             "options": {
                 "force": true
@@ -77,12 +81,12 @@
         };
         callback();
     });
-    
+
     this.Given(/^the build step "([^"]*)" has a task group "([^"]*)"$/, function (stepName, groupName, callback) {
         config[stepName][groupName] = {};
         callback();
     });
-    
+
     this.Given(/^the task group "([^"]*)" in the build step "([^"]*)" has a task runner that copies the src directory to the target directory$/, function (groupName, stepName, callback) {
         config[stepName][groupName]["copy"] = {
             "task": "copy",
@@ -101,8 +105,33 @@
         callback();
     });
     
+    
+
+    this.Given(/^the task group "([^"]*)" in the build step "([^"]*)" has a task runner that copies the src directory to the target directory and calls the function "([^"]*)" from script "([^"]*)"$/, function (groupName, stepName, funct, script, callback) {
+        config[stepName][groupName]["copy"] = {
+            "task": "copy",
+            "package": "grunt-contrib-copy",
+            "deployables": {
+                "files": [
+                    {
+                        "expand": true, "flatten": true,
+                        "cwd": this.fileSystem.baseDirectory,
+                        "src": ["src/*.js"],
+                        "dest": "target/",
+                        "rename" : { 
+                            "serialized:type" : "Function",
+                            "serialized:object" : { "src": script },
+                            "serialized:call" : funct
+                        }
+                    }
+                ]
+            }
+        };
+        callback();
+    });
+
     this.Given(/^the module HelloWorld is defined and exists$/, function (callback) {
-        modules.push(require('path').join(this.fileSystem.baseDirectory, "HelloWorld.js"));
+        modules.push("HelloWorld.js");
         var self = this;
         this.fileSystem.readFile("../../support/HelloWorld.js", function (moduleContent) {
             self.fileSystem.withFileWithContentInDirectory("HelloWorld.js", moduleContent, ".", callback);
@@ -110,37 +139,37 @@
     });
 
     this.Given(/^the build step "([^"]*)" has a exec task runner with the command "([^"]*)"$/, function (buildStepName, command, callback) {
-      config = {
-          buildStepName : {
-            "hello world" : {
-                "task": "exec",
-                "package": "grunt-exec",
-                "echo-module" : {
-                    "cmd" : command
-                }
-            }
-         }
-      };
-      callback();
-    });
-    
-    this.Given(/^the build step echo has a exec task runner with a type discriminator for the HelloWorld Module with "(.*)" as actor$/, function (arg1, callback) {
         config = {
-        "test-module": {
-            "hello world" : {
-                "task": "exec",
-                "package": "grunt-exec",
-                "echo-module" : {
-                    "cmd" : {
-                        "serialized:type" : "modules.HelloWorld",
-                        "serialized:object" : "{ \"defaultPersonToGreet\" : \"" + arg1 + "\"  }",
-                        "serialized:call" : "greet"
+            buildStepName: {
+                "hello world": {
+                    "task": "exec",
+                    "package": "grunt-exec",
+                    "echo-module": {
+                        "cmd": command
                     }
                 }
             }
-         }
-      };
-      callback();
+        };
+        callback();
+    });
+
+    this.Given(/^the build step echo has a exec task runner with a type discriminator for the HelloWorld Module with "(.*)" as actor$/, function (arg1, callback) {
+        config = {
+            "test-module": {
+                "hello world": {
+                    "task": "exec",
+                    "package": "grunt-exec",
+                    "echo-module": {
+                        "cmd": {
+                            "serialized:type": "modules.HelloWorld",
+                            "serialized:object": "{ \"defaultPersonToGreet\" : \"" + arg1 + "\"  }",
+                            "serialized:call": "greet"
+                        }
+                    }
+                }
+            }
+        };
+        callback();
     });
 
     this.Given(/^the variable "([^"]*)" exists under "([^"]*)"$/, function (innerVariable, outerVariable, callback) {
@@ -155,11 +184,11 @@
     this.When("I execute the build command", function (callback) {
         this.terminal.execute("../../../../build", callback);
     });
-    
+
     this.When(/^I execute a custom build command with argument "([^"]*)"$/, function (arg, callback) {
         this.terminal.execute("../../../../build " + arg, callback);
     });
-    
+
     this.Then(/^all the steps should be executed in the precise order$/, function (callback) {
         var template = 'Running "{what}" task';
         var _ = this;
@@ -171,16 +200,16 @@
             var name = (parents) ? parents + "/" + item : item;
             tasks.push(template.replace("{what}", name));
             if (node["task"]) return;
-            
+
             var children = Object.keys(node);
             children.forEach(function (child) {
                 traverse(node, child, name);
             });
-        };      
-        steps.forEach(function(step) {  
+        };
+        steps.forEach(function (step) {
             traverse(config, step);
         });
-        
+
         var lastIndex = -1;
         tasks.forEach(function (task) {
             _.terminal.output.should.contain(task);
@@ -191,30 +220,30 @@
 
         callback();
     });
-    
+
     this.Then(/^the file "([^"]*)" should exist in folder "([^"]*)"$/, function (fileName, folder, callback) {
         this.fileSystem.isFileInDirectory(fileName, folder).should.be.true;
         callback();
     });
 
     this.Then(/^the message "([^"]*)" should appear on the command line$/, function (arg1, callback) {
-      var _ = this;
-      _.terminal.output.should.contain(arg1);
-      callback();
+        var _ = this;
+        _.terminal.output.should.contain(arg1);
+        callback();
     });
 
     this.Then(/^the step "([^"]*)" should have been executed$/, function (task, callback) {
-      var expected = "Running \""+ task + "\" task";
-      var _ = this;
-      _.terminal.output.should.contain(expected);
-      callback();
+        var expected = "Running \"" + task + "\" task";
+        var _ = this;
+        _.terminal.output.should.contain(expected);
+        callback();
     });
-    
+
     this.Then(/^the step "([^"]*)" should not have been executed$/, function (task, callback) {
-      var not_expected = "Running \""+ task + "\" task";
-      var _ = this;
-      _.terminal.output.should.not.contain(not_expected);
-      callback();
+        var not_expected = "Running \"" + task + "\" task";
+        var _ = this;
+        _.terminal.output.should.not.contain(not_expected);
+        callback();
     });
 };
 
